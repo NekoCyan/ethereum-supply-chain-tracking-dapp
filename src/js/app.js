0f4794a9347841f1eaf9856b41c8221541cb9215
NekoCyan <cyanhijirikawa88@gmail.com>
2025-04-16 02:53:33 +0700

    feat: latest.

--- src/js/app.js
+++ src/js/app.js
@@ -124,9 +124,7 @@ App = {
     handleButtonClick: async function (event) {
         event.preventDefault();
 
-        App.getMetaskAccountID();
-
-        var processId = parseInt($(event.target).data('id'));
+        const processId = parseInt($(event.target).data('id'));
         console.log('processId', processId);
 
         switch (processId) {
@@ -175,7 +173,8 @@ App = {
                 App.originFarmInformation,
                 App.originFarmLatitude,
                 App.originFarmLongitude,
-                App.productNotes
+                App.productNotes,
+                { from: App.metamaskAccountID }
             );
         }).then(function (result) {
             $("#ftc-item").text(result);
@@ -184,6 +183,7 @@ App = {
             console.log(err.message);
         });
     },
+
 
     processItem: function (event) {
         event.preventDefault();
@@ -218,7 +218,7 @@ App = {
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function (instance) {
-            const productPrice = web3.toWei(1, "ether");
+            const productPrice = web3.utils.toWei("1", "ether");
             console.log('productPrice', productPrice);
             return instance.sellItem(App.upc, App.productPrice, { from: App.metamaskAccountID });
         }).then(function (result) {
@@ -234,7 +234,7 @@ App = {
         var processId = parseInt($(event.target).data('id'));
 
         App.contracts.SupplyChain.deployed().then(function (instance) {
-            const walletValue = web3.toWei(3, "ether");
+            const walletValue = web3.utils.toWei("3", "ether")
             return instance.buyItem(App.upc, { from: App.metamaskAccountID, value: walletValue });
         }).then(function (result) {
             $("#ftc-item").text(result);
